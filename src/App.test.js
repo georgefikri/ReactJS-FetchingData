3683e64e13ce6ae295fcb757c58d7a54fa811142
George Fikri <40975734+georgefikri@users.noreply.github.com>
2025-07-26 17:02:05 +0300

    Add custom fetch hook

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ name: 'Luke Skywalker' })
+    })
+  );
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  global.fetch.mockClear();
+  delete global.fetch;
 });
